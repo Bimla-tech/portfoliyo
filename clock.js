8b1a37898facc2caf8a0d55ae66fa6110b03ffb0
Bimla-tech <godarabimla010@gmail.com>
2026-02-01 01:41:19 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,30 @@
+
+
+const hour=document.getElementById("hour");
+const minute=document.getElementById("minute");
+const second=document.getElementById("second");
+let h,m,s;
+function setTime(){
+    let date=new Date();
+    h=date.getHours();
+    m=date.getMinutes();
+    s=date.Seconds();
+
+}
+ function setHands(){
+    if(h>12){
+        h=h-12;
+
+    }
+    let hRotation=h*30+m*0.5;
+    let mrotation=6*m;
+    let sRotation=6*s;
+   
+    hour.style.transform='rotate(${hRotation}deg)'
+    minute.style.transform='rotate(${mRotation}deg)'
+    second.style.transform='rotate(${sRotation}deg)'
+ }
+ setInterval(()=>{
+    setTime();
+    setHands();
+ },1000);
---
+++ contry.js
@@ -0,0 +1,22 @@
+const countries ={
+"af":"Afghanistan",
+	"al":"Albania",
+	"alg":"Algeria",
+	"am":"American Samoa",
+	"an":"Andorra",
+	"an":"Angola",
+	"An":"Anguilla",
+
+	
+	
+	
+	
+		"vi":"Virgin Islands (British)",
+	"Vi":"Virgin Islands (U.S.)",
+	"wi":"Wallis and Futuna",
+	"we":"Western Sahara",
+	"ye":"Yemen",
+	"za":"Zambia",
+	"zi":"Zimbabwe",
+	"al-is":"Åland Islands"
+}
